b866d2fba7599027e8f7e5ec7556b8ab7f926416
Alvin Wang <alvin@cask.co>
2014-10-07 17:23:30 -0700

    Addressed comments

--- common-cli/src/main/java/co/cask/cdap/common/cli/Arguments.java
+++ common-cli/src/main/java/co/cask/cdap/common/cli/Arguments.java
@@ -23,11 +23,20 @@ import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 
 /**
- * Convenience class to read from a set of arguments.
+ * Convenience class to represent a set of arguments contained within user input.
+ *
+ * For example, if the user input is "create dataset test", and the argument
+ * is "test" and is called "dataset-name", then the {@link #arguments} map
+ * would contain "dataset-name" -> "test" and the {@link #rawInput}
+ * would be "create dataset test".
  */
 public class Arguments {
 
   private final Map<String, String> arguments;
+  /**
+   * The full raw input that the user entered. Useful for commands that
+   * don't necessarily expect a single value per argument.
+   */
   private final String rawInput;
 
   public Arguments(Map<String, String> arguments, String rawInput) {
@@ -35,32 +44,52 @@ public class Arguments {
     this.arguments = ImmutableMap.copyOf(arguments);
   }
 
+  /**
+   * @return the raw user input
+   */
   public String getRawInput() {
     return rawInput;
   }
 
+  /**
+   * @param key the argument name
+   * @return the argument value
+   * @throws MissingArgumentException if the argument value is missing
+   */
   public String get(String key) throws MissingArgumentException {
-    String value = arguments.get(key);
-    if (value == null) {
-      throw new MissingArgumentException("Missing required argument: " + key);
-    }
-
-    return value;
+    checkRequiredArgument(key);
+    return arguments.get(key);
   }
 
+  /**
+   * @param key the argument name
+   * @param defaultValue the value to return if the argument is missing
+   * @return the argument value
+   */
   public String get(String key, String defaultValue) {
     String value = arguments.get(key);
     return Objects.firstNonNull(value, defaultValue);
   }
 
-  public Integer getInt(String key) throws MissingArgumentException {
-    String value = arguments.get(key);
-    if (value == null) {
-      throw new MissingArgumentException("Missing required argument: " + key);
-    }
-    return Integer.parseInt(value);
+  /**
+   * Gets an argument value as an integer.
+   *
+   * @param key the argument name
+   * @return the argument value
+   * @throws MissingArgumentException if the argument value is missing
+   */
+  public int getInt(String key) throws MissingArgumentException {
+    checkRequiredArgument(key);
+    return Integer.parseInt(arguments.get(key));
   }
 
+  /**
+   * Gets an argument value as an integer.
+   *
+   * @param key the argument name
+   * @param defaultValue the value to return if the argument is missing
+   * @return the argument value
+   */
   public Integer getInt(String key, int defaultValue) {
     String value = arguments.get(key);
     if (value != null) {
@@ -70,20 +99,37 @@ public class Arguments {
     }
   }
 
+  /**
+   * Gets an argument value as a long.
+   *
+   * @param key the argument name
+   * @return the argument value
+   * @throws MissingArgumentException if the argument value is missing
+   */
   public Long getLong(String key) throws MissingArgumentException {
-    String value = arguments.get(key);
-    if (value == null) {
-      throw new MissingArgumentException("Missing required argument: " + key);
-    }
-    return Long.parseLong(value);
+    checkRequiredArgument(key);
+    return Long.parseLong(arguments.get(key));
   }
 
+  /**
+   * Gets an argument value as a long.
+   *
+   * @param key the argument name
+   * @param defaultValue the value to return if the argument is missing
+   * @return the argument value
+   */
   public Long getLong(String key, long defaultValue) {
     String value = arguments.get(key);
     if (value != null) {
       return Long.parseLong(value);
     } else {
       return defaultValue;
+    }
+  }
+
+  private void checkRequiredArgument(String key) throws MissingArgumentException {
+    if (!arguments.containsKey(key)) {
+      throw new MissingArgumentException("Missing required argument: " + key);
     }
   }
 }
