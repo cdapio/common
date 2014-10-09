079cda250bf00ba9ab7eccaf82f615b0b566c400
Yurii Talashko <ytalashko@continuuity.com>
2014-10-09 09:51:32 -0400

    common-cli: improve arguments parsing

--- common-cli/src/main/java/co/cask/common/cli/CommandMatch.java
+++ common-cli/src/main/java/co/cask/common/cli/CommandMatch.java
@@ -18,6 +18,9 @@ package co.cask.common.cli;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Represents an input matching for a command and provided arguments.
  */
@@ -50,28 +53,84 @@ public final class CommandMatch {
   }
 
   private Arguments parseArguments(String input, String pattern) {
-    String[] patternTokens = pattern.split(" ");
-    String[] inputTokens = input.split(" ");
-
     ImmutableMap.Builder<String, String> args = ImmutableMap.builder();
-
-    for (int i = 0; i < patternTokens.length; i++) {
-      String patternToken = patternTokens[i];
-      if (patternToken.startsWith("<") && patternToken.endsWith(">")) {
-        // required argument
-        args.put(patternToken.substring(1, patternToken.length() - 1), inputTokens[i]);
-      } else if (patternToken.startsWith("[") && patternToken.endsWith("]")) {
-        // optional argument
-        if (i < inputTokens.length) {
-          args.put(patternToken.substring(1, patternToken.length() - 1), inputTokens[i]);
-        }
-      } else {
-        if (!patternToken.equals(inputTokens[i])) {
-          throw new IllegalArgumentException("Expected format: " + pattern);
-        }
-      }
+    int mandatoryEnd = pattern.indexOf("[");
+    mandatoryEnd = mandatoryEnd > 0 ? mandatoryEnd : pattern.length();
+    String mandatoryPatternPart = pattern.substring(0, mandatoryEnd).trim();
+    boolean isFullPattern = mandatoryEnd == pattern.length();
+    int currentIndex = parseArgs(args, input, mandatoryPatternPart, isFullPattern);
+    if (isFullPattern) {
+      return new Arguments(args.build(), input);
     }
 
+    String rawOptionalPatternPart = pattern.substring(mandatoryEnd).trim();
+    rawOptionalPatternPart = rawOptionalPatternPart.replace("[", "");
+    String[] rawOptionalArgs = rawOptionalPatternPart.split("]");
+    String optionalPatternPart = sortAndFilter(input.substring(currentIndex), rawOptionalArgs);
+    String lastMandatoryValue = input.substring(currentIndex, input.indexOf(
+      optionalPatternPart.substring(0, optionalPatternPart.indexOf("<")), currentIndex));
+    args.put(mandatoryPatternPart.substring(mandatoryPatternPart.lastIndexOf("<") + 1,
+                                            mandatoryPatternPart.lastIndexOf(">")), lastMandatoryValue);
+
+    currentIndex += lastMandatoryValue.length();
+    parseArgs(args, input.substring(currentIndex), optionalPatternPart, true);
     return new Arguments(args.build(), input);
+  }
+
+  private int parseArgs(ImmutableMap.Builder<String, String> args, String input,
+                    String pattern, boolean isFullPattern) {
+    String[] patternArgs = pattern.split(">");
+    int currentIndex = 0;
+    for (int i = 0; i < patternArgs.length - 1; i++) {
+      String[] currentOption = patternArgs[i].split("<");
+      String[] nextOption = patternArgs[i + 1].split("<");
+      if (!input.startsWith(currentOption[0], currentIndex)) {
+        throw new IllegalArgumentException("Expected format: " + command.getPattern());
+      }
+      currentIndex += currentOption[0].length();
+      int argEnd = input.indexOf(nextOption[0], currentIndex);
+      if (argEnd == -1 || currentIndex == argEnd) {
+        throw new IllegalArgumentException("Expected format: " + command.getPattern());
+      }
+      String value = input.substring(currentIndex, argEnd);
+      args.put(currentOption[1], value);
+      currentIndex += value.length();
+    }
+    currentIndex += patternArgs[patternArgs.length - 1].split("<")[0].length();
+    if (isFullPattern) {
+      if (currentIndex == input.length()) {
+        throw new IllegalArgumentException("Expected format: " + command.getPattern());
+      }
+      args.put(patternArgs[patternArgs.length - 1].split("<")[1], input.substring(currentIndex));
+    }
+    return currentIndex;
+  }
+
+  private String sortAndFilter(final String input, String[] args) {
+    String[] argsCopy = new String[args.length];
+    System.arraycopy(args, 0, argsCopy, 0, args.length);
+    Arrays.sort(argsCopy, new Comparator<String>() {
+
+      @Override
+      public int compare(String arg1, String arg2) {
+        int arg2Length = input.indexOf(arg2.split("<")[0]);
+        if (arg2Length == -1) {
+          return -1;
+        }
+        int arg1Length = input.indexOf(arg1.split("<")[0]);
+        if (arg1Length == -1) {
+          return 1;
+        }
+        return arg1Length - arg2Length;
+      }
+    });
+    StringBuilder builder = new StringBuilder();
+    for (String arg : argsCopy) {
+      if (!input.contains(arg.split("<")[0])) {
+        break;
+      }
+      builder.append(arg);
+    }
+    return builder.toString();
   }
 }
