11e21d23f570d552b7662038a578f4df3c987741
Yurii Talashko <ytalashko@continuuity.com>
2014-11-28 11:27:16 -0500

    CDAP-531: add inbuilt Exit, Quit and Help Commands

--- common-cli/src/main/java/co/cask/common/cli/CommandSet.java
+++ common-cli/src/main/java/co/cask/common/cli/CommandSet.java
@@ -19,6 +19,7 @@ package co.cask.common.cli;
 import co.cask.common.cli.exception.InvalidCommandException;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 import java.util.Iterator;
 import java.util.List;
@@ -71,6 +72,22 @@ public class CommandSet<T extends Command> implements Iterable<T> {
     }
 
     throw new InvalidCommandException(input);
+  }
+
+  /**
+   * Finds a matching commands for the provided input.
+   *
+   * @param input the input string
+   * @return the matching commands
+   */
+  public List<T> findMatchCommands(String input) {
+    List<T> appropriateCommands = Lists.newArrayList();
+    for (T command : this) {
+      if (command.getPattern().startsWith(input)) {
+        appropriateCommands.add(command);
+      }
+    }
+    return appropriateCommands;
   }
 
   public Iterable<T> getCommands() {
